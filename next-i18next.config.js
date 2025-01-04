6acc75e2805c0d6c6b3bdc327c53caba37d6453a
LH <192437136+Leon-Hoeck@users.noreply.github.com>
2025-01-04 22:05:52 +0100

    v3.01 reupload 25.5 Completion of basic design

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
-    locales: ['en', 'de'],
-    defaultLocale: 'en', // Set 'de' as the default locale
-    localeDetection: false, // Prevent automatic detection
+    locales: ['en', 'de', 'fr', 'it'],
+    defaultLocale: 'en',
+    localeDetection: false,
   },
   localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
